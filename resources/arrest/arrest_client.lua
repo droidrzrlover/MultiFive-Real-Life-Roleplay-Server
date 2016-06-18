4deebb30ed7f0cce1c3a2f19cbb23041a9b76170
cody196 <cody.wherry@gmail.com>
2016-06-17 23:27:20 -0400

    added more of script, needs looking over

--- resources/arrest/arrest_client.lua
+++ resources/arrest/arrest_client.lua
@@ -1,3 +1,11 @@
-function arrest(getPlayerID, thePlayer)
-	GET_PLAYER_WANTED_LEVEL(thePlayer)
-if  
+function arrest()
+	GET_PLAYER_WANTED_LEVEL(getPlayerID, thePlayer)
+	if thePlayer(isWanted)
+	then displayName
+end
+
+function tazed()
+	weapon("stun_gun")
+	if playerIsHit
+	then freezePlayer(x, y, z)
+	vector3.playerPosition.getModel.getPosition
