09bb2406c549a9b6e5e663c00a71e0f17980ef35
cody196 <cody.wherry@gmail.com>
2016-06-18 15:53:43 -0400

    Update localchat.lua

--- resources/localchat/localchat.lua
+++ resources/localchat/localchat.lua
@@ -1,1 +1,32 @@
-function localchat()
+chat_range=100
+ 
+addEventHandler("onPlayerJoin",getRootElement(),
+function ()
+bindKey(source,"y","down","chatbox","Local")
+end)
+ 
+addEventHandler("onResourceStart",getResourceRootElement(getThisResource()),
+function ()
+for index, player in pairs(getElementsByType("player")) do
+bindKey(player,"y","down","chatbox","Local")
+  end
+end)
+ 
+function isPlayerInRangeOfPoint(player,x,y,z,range)
+   local px,py,pz=getElementPosition(player)
+   return ((x-px)^2+(y-py)^2+(z-pz)^2)^0.5<=range
+end
+ 
+function onChat(player,_,...)
+  local px,py,pz=getElementPosition(player)
+  local msg = table.concat({...}, " ")
+  local nick=getPlayerName(player)
+local r,g,b = getTeamColor(getPlayerTeam(player))
+  for _,v in ipairs(getElementsByType("player")) do
+    if isPlayerInRangeOfPoint(v,px,py,pz,chat_range) then
+      outputChatBox("(Local) "..nick..": "..msg,v,r,g,b,true)
+    end
+  end
+end
+addCommandHandler("Local",onChat)
+ 
