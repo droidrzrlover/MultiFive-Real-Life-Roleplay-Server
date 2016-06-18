bfa675ecca559d1c5cd551a735928011749d45c5
cody196 <cody.wherry@gmail.com>
2016-06-18 00:27:33 -0400

    Updated

--- resources/arrest/arrest_client.lua
+++ resources/arrest/arrest_client.lua
@@ -1,13 +1,20 @@
-function arrest()
+function checkWantedLevel()
 	GET_PLAYER_WANTED_LEVEL(getPlayerID, thePlayer)
 	if thePlayer(isWanted)
 	then displayName
 	else GET_TIME_SINCE_LAST_ARREST("_time_, _reason_")
-
 end
 
 function tazed()
 	weapon("stun_gun")
-	if playerIsHit
+	if playerHit
 	then freezePlayer(x, y, z)
 	vector3.playerPosition.getModel.getPosition
+
+function arrest()
+	IS_PLAYER_BEING_ARRESTED(thePlayer)
+	PLAY_ENTITY_ANIM("random@arrests@busted")
+	IS_PED_CUFFED(thePlayer)
+	PLAY_ENTITY_ANIM("get_up@cuffed")
+	SET_PLAYER_WANTED_LEVEL(int)
+	SET_PLAYER_WANTED_LEVEL_now(0)
