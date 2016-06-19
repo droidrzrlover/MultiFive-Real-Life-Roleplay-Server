8e4ffdbe25ddad6b8a12ba54d226efae062cc9e8
cody196 <cody.wherry@gmail.com>
2016-06-18 20:32:37 -0400

    Added Server-side wanted check

--- resources/arrest/arrest_server.lua
+++ resources/arrest/arrest_server.lua
@@ -1,1 +1,9 @@
+local players = getElementsByType ( "player" ) -- get a table of all the players in the server
+for theKey,thePlayer in ipairs(players) do -- use a generic for loop to step through each player
+   local level = getPlayerWantedLevel ( thePlayer ) -- get the wanted level of the player
+   if ( level > 0 ) then -- if the player has any stars, announce it in the chat:
+      outputChatBox ( getPlayerName ( thePlayer ) .. " has a wanted level of " .. level .. "  stars!" )
+   end 
+end
+
 
