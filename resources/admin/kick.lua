2e27bfefccb1bdd5273e7f6f29999781b4d93d69
cody196 <cody.wherry@gmail.com>
2016-06-23 19:50:33 -0400

    Update kick.lua

--- resources/admin/kick.lua
+++ resources/admin/kick.lua
@@ -1,1 +1,5 @@
-function kickPlayer()
+function kickPlayer(command, theClient, reason)
+  if ( hasObjectPermissionTo ( theClient, "function.kickPlayer" ) ) then
+		--Get player element from the name
+		local kickedPlayer = getPlayerFromName ( kickedName )
+	  showNotification ( "Kick:" .. kickedName .. " has been kicked " ..Reason.. )
