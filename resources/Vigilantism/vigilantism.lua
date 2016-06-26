aec64502137187d50156595a64072f917faf17be
cody196 <cody.wherry@gmail.com>
2016-06-26 18:50:40 -0400

    Update vigilantism.lua

--- resources/Vigilantism/vigilantism.lua
+++ resources/Vigilantism/vigilantism.lua
@@ -1,1 +1,10 @@
+local vigilante()
+local callout()
 
+
+citizen.function vigilante (keyPressed, playerName, theCrime)
+  addEventHandler ( 'F11', isKeyPressed (true) )
+else false
+end
+
+citizen.function callout (toPlayer, theCrime, location)
